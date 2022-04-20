2fb6a19abb168aa69caab4c98b0941f74ef2fdd8
Sergey <originalustinov@gmail.com>
2022-04-21 01:03:13 +0300

    Another iteration with views

--- 01_docker_compose/app/movies/api/v1/views.py
+++ 01_docker_compose/app/movies/api/v1/views.py
@@ -14,39 +14,44 @@ class MoviesApiMixin:
     model = FilmWork
     http_method_names = ['get']
 
-    def get_queryset(self) -> QuerySet[FilmWork]:
-        return self.model.objects.all()
-
     def render_to_response(self, context, **response_kwargs):
         return JsonResponse(context)
 
 
-class MoviesListApi(BaseListView, MoviesApiMixin):
+class MoviesListApi(MoviesApiMixin, BaseListView):
     paginate_by = MOVIES_PER_PAGE
 
-    # def get_queryset(self) -> QuerySet[FilmWork]:
-    #     return self.model.objects.all()
+    def get_queryset(self) -> QuerySet[FilmWork]:
+        return self.model.objects.all()
 
     def get_context_data(self, *, object_list=None, **kwargs) -> MoviesList:
+        page_number = int(self.request.GET.get('page', "0"))
         queryset = self.get_queryset()
         paginator, page, queryset, is_paginated = self.paginate_queryset(
             queryset,
             self.paginate_by
         )
 
+        pg = paginator.get_page(page_number)
         context = {
             'count': paginator.count,
-            "total_pages": 0,
-            "prev": 0,
-            "next": 0,
-            'results': list(queryset),
+            "total_pages": paginator.num_pages,
+            "prev": pg.previous_page_number(),
+            "next": pg.next_page_number(),
+            'results': pg.object_list,
         }
         return context
 
 
 class MoviesDetailApi(MoviesApiMixin, BaseDetailView):
 
-    def get_context_data(self, **kwargs):
-        return {
-            "comments": "no"
+    def get_queryset(self) -> QuerySet[FilmWork]:
+        return self.model.objects.filter(pk=self.kwargs.get('pk'))
+
+    def get(self, request, pk, *args, **kwargs):
+        queryset = self.get_queryset()
+        movie = queryset.get()
+        context = {
+            "title": movie.title,
         }
+        return JsonResponse(context)
